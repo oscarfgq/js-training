a1f51106a895628dc2fc4b989b87e648d4e4d663
Oscar Gonzalez <oscar.gonzalez@nearshorecode.com>
2017-12-06 18:10:39 -0400

    Checking "hoisting" in checklist.

--- js-recap.js
+++ js-recap.js
@@ -1,6 +1,25 @@
 'use strict';
 
-/** Checklist:
-  * Hoisting
-  * Closures
+/** JS checklist:
+  * [OK] Hoisting
+  * [OK] Closures
+  * [  ] this
+  * [  ] delete
+  * [  ] Spread and Destructuring
+  * [  ] call, apply, arguments
+  * [  ] import, export (default)
+  * [  ] var, const, let (types)
+  * [  ] Comparison operators: (e.g. == vs. ===)
+  * [  ] try, catch
+  * [  ] array functions: (map, find, reduce, filter, concat, slice)
+  * [  ] arrow functions
+  * [  ] callback hell
+  * [  ] string interpolation
+  * [  ] strict mode
+  * [  ] Promises (creation, consuming and related elements: async/await, callbacks)
+  * [  ] IIFE
+  * [  ] Execution contexts
+  * [  ] binding
+  * [  ] Headers, methods and body payloads
+  * [  ] fetch / XHTTPRequest
   */
