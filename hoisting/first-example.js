766fe9ae17cf880dbaaa15875c0a315a87b732be
Oscar Gonzalez <oscar.gonzalez@nearshorecode.com>
2017-12-06 18:26:36 -0400

    Other topix examples cleanup.

--- hoisting/first-example.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Example 1 */
-catName('Chloe');
-
-function catName(name) {
-  console.log('My cat\'s name is ' + name);  // My cat's name is Chloe -- hoists function declaration
-}
